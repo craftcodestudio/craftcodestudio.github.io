6a63469c0268e5c918f479c0a0748754d6fc281f
Konstantin Kreft <konstantin.kreft@gmail.com>
2016-12-28 17:45:04 +0100

    Adding existing files

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+(function($){
+
+	/* ---------------------------------------------- /*
+	 * Preloader
+	/* ---------------------------------------------- */
+
+	$(window).load(function() {
+		$('#status').fadeOut();
+		$('#preloader').delay(0).fadeOut('slow');
+	});
+
+})(jQuery);
